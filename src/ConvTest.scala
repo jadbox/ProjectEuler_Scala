ca082a8e35b31e396cbcb7ed800a019f186d241b
Jonathan Dunlap <jadit2@gmail.com>
2014-06-24 08:02:39 -0700

    added conversion test

---
+++ src/ConvTest.scala
@@ -0,0 +1,13 @@
+object ConvTest {
+  def med[T <: AnyVal{ def toInt:Int;} ](x:Array[T]) = {
+    ((x(x.length/2-1).toInt + x(x.length/2).toInt)/2).toChar;
+  }
+  
+  def main(args: Array[String]): Unit = {
+    var l = Array[Char]('a', 'b', 'd', 'e')
+    l = l.sorted
+    println ( med(l) );
+    
+  }
+
+}
